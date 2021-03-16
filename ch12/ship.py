b0ba9a9a183497314f857dab7a735fc15700b21a
Edgar Saavedra <edgarsaavedraa@gmail.com>
2021-03-15 22:26:16 -0700

    chap 12 finished

--- ch12/ship.py
+++ ch12/ship.py
@@ -5,12 +5,28 @@ class Ship:
   def __init__(self, ai_game):
     """Init the ship and set it starting position"""
     self.screen = ai_game.screen
+    self.settings = ai_game.settings
+
     self.screen_rect = ai_game.screen.get_rect()
 
     # Load the ship image and get its rect (size)
     self.image = pygame.image.load('images/ship.bmp')
     self.rect = self.image.get_rect()
     self.rect.midbottom = self.screen_rect.midbottom
+
+    self.x = float(self.rect.x)
+
+    self.moving_right = False
+    self.moving_left = False
+  
+  def update(self):
+    """Update the ships position based on the movement flag."""
+    if self.moving_right and self.rect.right < self.screen_rect.right:
+      self.x += self.settings.ship_speed
+    if self.moving_left and self.rect.left > 0:
+      self.x -= self.settings.ship_speed
+    # updte rect object
+    self.rect.x = self.x
   
   def blitme(self):
     """Draw the ship at its current location"""
