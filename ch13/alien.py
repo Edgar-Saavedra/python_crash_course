334576e140c7ef025167ef2ff777413159fb0b2f
Edgar Saavedra <edgarsaavedraa@gmail.com>
2021-03-17 22:44:59 -0700

    add collision and blow up aliens

---
+++ ch13/alien.py
@@ -0,0 +1,27 @@
+import pygame
+from pygame.sprite import Sprite
+
+class Alien(Sprite):
+  """A class to represent a single alien in the fleet."""
+  
+  def __init__(self, ai_game):
+    """Initialize the alien and set its starting position."""
+    super().__init__()
+    self.screen = ai_game.screen
+    self.settings = ai_game.settings
+
+    # load the alien image and set its rect attribute
+    self.image = pygame.image.load('images/alien.bmp')
+    self.rect = self.image.get_rect()
+
+    # start each new alien near the top left of the screen.
+    self.rect.x = self.rect.width
+    self.rect.y = self.rect.height
+
+    # store the aliens exact hors pos
+    self.x = float(self.rect.x)
+  
+  def update(self): 
+    """MOve the alien to the right."""
+    self.x += self.settings.alien_speed
+    self.rect.x = self.x
